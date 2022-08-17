dd314738334c9acb5dda7f8896f26772961a4ab5
AnnaSemichaeva <tdhnss@gmail.com>
2022-08-17 22:29:56 +0300

    Data saving is implemented using the UserDefaults class

--- ToDoList/Model.swift
+++ ToDoList/Model.swift
@@ -7,30 +7,36 @@
 
 import Foundation
 
-var toDoItems: [[String: Any]] = [["Name": "Позвонить маме", "isCompleted": false], ["Name": "Написать приложение", "isCompleted": true]]
+var toDoItems: [[String: Any]] {
+    set {
+        UserDefaults.standard.set(newValue, forKey: "toDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    
+    get {
+        if let array = UserDefaults.standard.array(forKey: "toDoDataKey") as? [[String: Any]] {
+            return array
+        } else {
+            return []
+        }
+    }
+}
 
 func addItem(nameItem: String, isCompleted: Bool = false) {
     toDoItems.append(["Name": nameItem, "isCompleted": isCompleted])
-    saveData()
+    
 }
 
 func removeItem(at index: Int) {
     toDoItems.remove(at: index)
-    saveData()
+    
 }
 
 func changeState(at item: Int) -> Bool {
     toDoItems[item]["isCompleted"] = !(toDoItems[item]["isCompleted"] as! Bool)
-    saveData()
+    
     
     return toDoItems[item]["isCompleted"] as! Bool
     
 }
 
-func saveData() {
-    print("Save Data!")
-}
-
-func loadData() {
-    
-}
