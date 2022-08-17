03b63fb7f7a43e61c64a4e96d6573df00df4a949
AnnaSemichaeva <tdhnss@gmail.com>
2022-08-18 01:14:36 +0300

    minor changes

--- ToDoList/TableViewController.swift
+++ ToDoList/TableViewController.swift
@@ -112,12 +112,12 @@ class TableViewController: UITableViewController {
     
     
     
-    /*
-    // Override to support rearranging the table view.
+    
+     //Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
-    */
+    
 
     /*
     // Override to support conditional rearranging of the table view.
